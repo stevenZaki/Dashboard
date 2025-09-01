1a476ed3ec85de97a11a86d7197e504565132b84
unknown <stevensivo27@gmail.com>
2025-09-01 22:38:24 +0300

    Pagining

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -10,16 +10,18 @@ import { usePathname, useSearchParams } from 'next/navigation';
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
 
-  const allPages = generatePagination(currentPage, totalPages);
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
 
-  const createPageUrl = (pageNumber: number | string) => {
-    const params = new UrlSearchParams(searchParams);
+ 
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
-  }
+  };
 
   return (
     <>
